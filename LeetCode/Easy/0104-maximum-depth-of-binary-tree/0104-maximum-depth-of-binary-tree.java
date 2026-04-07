0c5f85f4defe7ba8e0e4ce1562ec4e9096744bf0
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2026-04-07 19:05:24 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -17,24 +17,31 @@ import java.util.*;
 
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
-
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.add(root);
-        int maxDepth=0;
-
-        while(!queue.isEmpty()){
-            maxDepth++;
-            int queueLen = queue.size();
-
-            while(queueLen-->0){
-                TreeNode node = queue.poll();
-
-                if(node.left != null) queue.add(node.left);
-                if(node.right != null) queue.add(node.right);
-            }
+        //dfs
+        if(root == null){
+            return 0;
         }
 
-        return maxDepth;
+        return 1+Math.max(maxDepth(root.left), maxDepth(root.right));
+        // //bfs
+        // if(root == null) return 0;
+
+        // Queue<TreeNode> queue = new ArrayDeque<>();
+        // queue.add(root);
+        // int maxDepth=0;
+
+        // while(!queue.isEmpty()){
+        //     maxDepth++;
+        //     int queueLen = queue.size();
+
+        //     while(queueLen-->0){
+        //         TreeNode node = queue.poll();
+
+        //         if(node.left != null) queue.add(node.left);
+        //         if(node.right != null) queue.add(node.right);
+        //     }
+        // }
+
+        // return maxDepth;
     }
 }
