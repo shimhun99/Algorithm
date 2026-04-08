f1e794303d1c22c3daef660d9a978fb072bc9d73
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2026-04-08 15:44:34 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ LeetCode/Easy/0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -17,25 +17,38 @@ import java.util.*;
 
 class Solution {
     Queue<TreeNode> queue = new ArrayDeque<>();
+    int minDepth=0;
 
     public int minDepth(TreeNode root) {
-        if(root == null) return 0;
+        if(root == null) return minDepth;
+        //dfs
+        // if(root.left==null&&root.right==null) return minDepth;
+        if(root.left==null && root.right==null) return 1+minDepth;
+        else if(root.left==null) return 1+minDepth(root.right);
+        else if(root.right==null) return 1+minDepth(root.left);
+        else return 1+Math.min(minDepth(root.left), minDepth(root.right));
 
-        queue.add(root);
-        int minDepth = 0;
+        // minDepth = 1+Math.min(minDepth(root.left), minDepth(root.right));
 
-        while(!queue.isEmpty()){
-            minDepth++;
-            int queueLen = queue.size();
+        // return minDepth;
+        // //bfs
+        // if(root == null) return 0;
 
-            while(queueLen-->0){
-                TreeNode node = queue.poll();
-                if(node.left==null && node.right==null) return minDepth;
-                if(node.left!=null) queue.add(node.left);
-                if(node.right!=null) queue.add(node.right);
-            }
-        }  
+        // queue.add(root);
+        // int minDepth = 0;
 
-        return minDepth;
+        // while(!queue.isEmpty()){
+        //     minDepth++;
+        //     int queueLen = queue.size();
+
+        //     while(queueLen-->0){
+        //         TreeNode node = queue.poll();
+        //         if(node.left==null && node.right==null) return minDepth;
+        //         if(node.left!=null) queue.add(node.left);
+        //         if(node.right!=null) queue.add(node.right);
+        //     }
+        // }  
+
+        // return minDepth;
     }
 }
