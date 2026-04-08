5a6ee5684ec13fa0800213e959943f706c9e1932
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2026-04-08 16:04:07 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ LeetCode/Easy/0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -1,58 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-import java.util.*;
-
 class Solution {
-    Queue<TreeNode> queue = new ArrayDeque<>();
-    int minDepth=0;
-
     public int minDepth(TreeNode root) {
-        if(root == null) return 0;
-        int left = minDepth(root.left);
-        int right = minDepth(root.right);
+        if (root == null) return 0;
 
-        if(left==0 || right==0) return Math.max(left,right)+1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 1;
 
-        return Math.min(left,right)+1;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
 
-        // //dfs
-        // if(root == null) return 0;
-        // if(root.left==null && root.right==null) return 1;
-        // else if(root.left==null) return 1+minDepth(root.right);
-        // else if(root.right==null) return 1+minDepth(root.left);
-        // else return 1+Math.min(minDepth(root.left), minDepth(root.right));
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null)
+                    return depth;
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
 
-        // //bfs
-        // if(root == null) return 0;
+            depth++;
+        }
 
-        // queue.add(root);
-        // int minDepth = 0;
-
-        // while(!queue.isEmpty()){
-        //     minDepth++;
-        //     int queueLen = queue.size();
-
-        //     while(queueLen-->0){
-        //         TreeNode node = queue.poll();
-        //         if(node.left==null && node.right==null) return minDepth;
-        //         if(node.left!=null) queue.add(node.left);
-        //         if(node.right!=null) queue.add(node.right);
-        //     }
-        // }  
-
-        // return minDepth;
+        return depth;
     }
 }
