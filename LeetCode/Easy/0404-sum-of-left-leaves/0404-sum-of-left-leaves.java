e729938a6358e7c7e815b7a26d6070aef8a14b55
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2026-04-09 04:53:19 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ LeetCode/Easy/0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -1,30 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    int total = 0; 
+
     public int sumOfLeftLeaves(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
         
-        Queue<Pair<TreeNode, Boolean>> queue = new LinkedList<>();
-        queue.offer(new Pair<>(root, false));  // (node, is_left)
-        int totalSum = 0;
-        
-        while (!queue.isEmpty()) {
-            Pair<TreeNode, Boolean> pair = queue.poll();
-            TreeNode node = pair.getKey();
-            boolean isLeft = pair.getValue();
-            
-            if (isLeft && node.left == null && node.right == null) {
-                totalSum += node.val;
-            }
-            
-            if (node.left != null) {
-                queue.offer(new Pair<>(node.left, true));
-            }
-            if (node.right != null) {
-                queue.offer(new Pair<>(node.right, false));
-            }
-        }
-        
-        return totalSum;
+        // //bfs
+        // queue.add(root);
+
+        // while(!queue.isEmpty()){
+        //     TreeNode node = queue.poll();
+        //     if(node.left!=null){
+        //         if(node.left.left==null && node.left.right==null) total+=node.left.val;
+        //         else queue.add(node.left);
+        //     }
+        //     if(node.right!=null) queue.add(node.right);
+        // }
+        // return total;
+
+        //dfs
+        // if(root == null) return 0;
+
+        // if(root.left!=null){
+        //     if(root.left.left==null && root.left.right==null) total+=root.left.val;
+        // }
+
+        // return total + sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
+        dfs(root);
+        return total;
+    }
+
+    void dfs(TreeNode node){
+        // if(node == null) return;
+        if(node.left!=null && node.left.left==null&&node.left.right==null) total+=node.left.val;
+
+        if(node.left!=null) dfs(node.left);
+        if(node.right!=null) dfs(node.right);
     }
 }
