62bc55f296e9097a1e7ce87a5f2f617880b8ad0b
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2026-04-09 04:07:32 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0226-invert-binary-tree/0226-invert-binary-tree.java
+++ LeetCode/Easy/0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -19,19 +19,30 @@ class Solution {
     public TreeNode invertTree(TreeNode root) {
         if(root == null) return null;
 
-        queue.add(root);
+        // //bfs
+        // queue.add(root);
 
-        while(!queue.isEmpty()){
-            TreeNode node = queue.poll();
-            TreeNode left = node.left;
-            TreeNode right = node.right;
+        // while(!queue.isEmpty()){
+        //     TreeNode node = queue.poll();
+        //     TreeNode left = node.left;
+        //     TreeNode right = node.right;
 
-            node.left=right;
-            node.right=left;
+        //     node.left=right;
+        //     node.right=left;
 
-            if(left!=null) queue.add(left);
-            if(right!=null) queue.add(right);
-        }
+        //     if(left!=null) queue.add(left);
+        //     if(right!=null) queue.add(right);
+        // }
+        // return root;
+
+        // dfs
+        TreeNode tempNode = root.left;
+        root.left = root.right;
+        root.right = tempNode;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
         return root;
     }
 }
