ab020cb2d586e5b8d8675212260425ec0a8fdeb6
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2026-04-09 04:30:52 +0900

    Time: 1 ms (8.84%), Space: 43.4 MB (38.37%) - LeetHub

---
+++ LeetCode/Easy/0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Queue<TreeNode> queue = new ArrayDeque<>();
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        int total = 0; 
+        
+        //bfs
+
+
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            if(node.left!=null){
+                if(node.left.left==null && node.left.right==null) total+=node.left.val;
+                else queue.add(node.left);
+            }
+            if(node.right!=null) queue.add(node.right);
+        }
+
+        return total;
+    }
+}
