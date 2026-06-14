44f25b3bee4317cbcee29034f6c5809ee138f595
Shim Hun <90015362+shimhun99@users.noreply.github.com>
2026-06-14 16:40:40 +0900

    Time: 1 ms (95.53%), Space: 42.8 MB (94.3%) - LeetHub

--- LeetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ LeetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -2,22 +2,32 @@ import java.util.*;
 
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int min_len = Math.min(word1.length(), word2.length());
+        // int min_len = Math.min(word1.length(), word2.length());
 
-        String result = "";
+        // String result = "";
 
-        for(int i=0; i<min_len; i++){
-            result+=word1.charAt(i);
-            result+=word2.charAt(i);
+        // for(int i=0; i<min_len; i++){
+        //     result+=word1.charAt(i);
+        //     result+=word2.charAt(i);
+        // }
+
+        // if(word1.length() > word2.length()){
+        //     result+=word1.substring(word2.length(), word1.length());
+        // }else{
+        //     result+=word2.substring(word1.length(), word2.length());
+        // }
+        // // System.out.println(result);
+
+        // return result;
+
+        int max_len = Math.max(word1.length(), word2.length());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<max_len; i++){
+            if(i<word1.length()) sb.append(word1.charAt(i));
+            if(i<word2.length()) sb.append(word2.charAt(i));
         }
 
-        if(word1.length() > word2.length()){
-            result+=word1.substring(word2.length(), word1.length());
-        }else{
-            result+=word2.substring(word1.length(), word2.length());
-        }
-        // System.out.println(result);
-
-        return result;
+        return sb.toString();
     }
 }
