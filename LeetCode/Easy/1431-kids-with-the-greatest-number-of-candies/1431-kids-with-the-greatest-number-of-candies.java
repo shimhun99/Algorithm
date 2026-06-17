8ab1e5c69575b33562cfd7e665d63a66931f2735
Shim Hun <90015362+shimhun99@users.noreply.github.com>
2026-06-17 23:52:45 +0900

    Time: 1 ms (96.19%), Space: 44 MB (42.69%) - LeetHub

--- LeetCode/Easy/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ LeetCode/Easy/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,23 +1,39 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        // List<Boolean> result = new ArrayList<>();
+
+        // for(int i=0; i<candies.length; i++){
+        //     int now = candies[i]+extraCandies;
+        //     int cnt = 0;
+
+        //     for(int j=0; j<candies.length; j++){
+        //         if(i==j) continue;
+        //         if(now < candies[j]){
+        //             result.add(false);
+        //             break;
+        //         }
+        //         cnt++; 
+        //     }
+
+        //     if(cnt == candies.length-1) result.add(true);
+        // }
+        
+        // return result;
+
+        int maxCandy = 0;
+
+        for(int candy : candies){
+            maxCandy = Math.max(candy, maxCandy);
+        }
+
+
         List<Boolean> result = new ArrayList<>();
 
-        for(int i=0; i<candies.length; i++){
-            int now = candies[i]+extraCandies;
-            int cnt = 0;
-
-            for(int j=0; j<candies.length; j++){
-                if(i==j) continue;
-                if(now < candies[j]){
-                    result.add(false);
-                    break;
-                }
-                cnt++; 
-            }
-
-            if(cnt == candies.length-1) result.add(true);
+        for(int candy : candies){
+            if(candy + extraCandies < maxCandy) result.add(false);
+            else result.add(true);
         }
-        
+
         return result;
     }
 }
