ee81091108872692f2e00c50b4a41875ee254599
Shim Hun <90015362+shimhun99@users.noreply.github.com>
2026-08-13 23:18:50 +0900

    Time: 26 ms (6.08%), Space: 20.5 MB (10.29%) - LeetHub

---
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sort_s = sorted(s.lower())
+        sort_t = sorted(t.lower())
+
+        print(sort_s)
+        print(sort_t)
+
+        return sort_s == sort_t
+        
