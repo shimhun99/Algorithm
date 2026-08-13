f15acb995e4118c0eb232297d06534cdb700ead6
Shim Hun <90015362+shimhun99@users.noreply.github.com>
2026-08-13 23:32:01 +0900

    Time: 15 ms (44.44%), Space: 20.3 MB (10.29%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # sort_s = sorted(s.lower())
-        # sort_t = sorted(t.lower())
+        # 1. sort    
+        return sorted(s) == sorted(t)
 
-        # print(sort_s)
-        # print(sort_t)
+        # 2. hash table
+        hash_table = {}
 
-        # return sort_s == sort_t
-
-        return sorted(s) == sorted(t)        
+        for ch in s:
+            if ch not in hash_table:
+                hash_table[ch] = 0
+            hash_table[ch] += 1
+        
+        for ch in t:
+            if ch not in hash_table:
+                return False
+            elif hash_table[ch] == 0:
+                del hash_table[ch]
+            else:
+                hash_table[ch] -= 1
+        return not hash_table
