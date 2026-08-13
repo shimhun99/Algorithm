8ebef21301c0d06998acd6e51dab463950ae59b9
Shim Hun <90015362+shimhun99@users.noreply.github.com>
2026-08-13 22:10:44 +0900

    Time: 7 ms (81.08%), Space: 20.1 MB (28.82%) - LeetHub

---
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # # 공백 제거
+        # s.strip()
+        
+        # 1. 문자열 공백 제거 + 문자/숫자가 아닌거 전부 제거
+        s = "".join(ch for ch in s if ch.isalnum())
+        print(s)
+
+        # 2. 소문자 변환
+        s = s.lower()
+        print(s)
+
+        # 3. 문자열 뒤집기
+        reverse_s = s[::-1]
+        print(reverse_s)
+
+
+        print(s)
+        print(reverse_s)
+        if s == reverse_s: 
+            return True
+        else:
+            return False
