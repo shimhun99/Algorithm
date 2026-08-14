60f35c1140ea9bd83c903c3c36c556432d133661
Shim Hun <90015362+shimhun99@users.noreply.github.com>
2026-08-14 23:52:05 +0900

    Time: 19 ms (19.24%), Space: 24 MB (16.43%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,21 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # anagrams = {}
+
+        # for word in strs:
+        #     sort_word = "".join(sorted(word))
+        #     if sort_word not in anagrams:
+        #         anagrams[sort_word] = list()
+        #     anagrams[sort_word].append(word)
+
+        # return list(anagrams.values())
+
         anagrams = {}
-
         for word in strs:
-            sort_word = "".join(sorted(word))
-            if sort_word not in anagrams:
-                anagrams[sort_word] = list()
-            anagrams[sort_word].append(word)
-
+            counter = [0] * 26
+            for ch in word:
+                counter[ord(ch) - ord('a')] += 1
+            if tuple(counter) not in anagrams:
+                anagrams[tuple(counter)] = []
+            anagrams[tuple(counter)].append(word)
         return list(anagrams.values())
-        print(anagrams)
