715535624fba185d2b451bc8cc58910d364d1141
Shim Hun <90015362+shimhun99@users.noreply.github.com>
2026-08-14 23:39:52 +0900

    Time: 11 ms (82.58%), Space: 22.2 MB (52.49%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
@@ -3,14 +3,10 @@ class Solution:
         anagrams = {}
 
         for word in strs:
-            # sort_word = sorted(word)
             sort_word = "".join(sorted(word))
-            # print(sort_word)
             if sort_word not in anagrams:
                 anagrams[sort_word] = list()
-            # else:
             anagrams[sort_word].append(word)
 
         return list(anagrams.values())
-        # print(anagrams.values())
         print(anagrams)
