8c642f507db6afaaf80a5f74304963359a5da0fa
Shim Hun <90015362+shimhun99@users.noreply.github.com>
2026-08-15 01:15:13 +0900

    Time: 3 ms (31.85%), Space: 19.3 MB (23.42%) - LeetHub

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.py
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,24 +5,15 @@ class Solution:
         brackets = {'(' : ')', '{' : '}', '[' : ']'}
 
         for bracket in s:
-            print("loop test")
-
-            if (bracket is '(') or (bracket is '[') or (bracket is '{'):
-                print(bracket)
+            if bracket in brackets:
+            # if (bracket is '(') or (bracket is '[') or (bracket is '{'):
                 stack.append(bracket)
 
             else:
                 if len(stack) is 0:
-                    # print(stack)
-                    print("test1")
                     return False
                 
                 if brackets[stack[-1]] != bracket:
-                    # print(stack)
-                    print(brackets[stack[-1]])
-                    print(bracket)
-                    print("test2")
                     return False
                 stack.pop()
-        print(stack)
         return len(stack) is 0
