7af6cce24a3a42a4f6a57f957ff5d249d8eb4f12
Shim Hun <90015362+shimhun99@users.noreply.github.com>
2026-09-01 23:09:29 +0900

    Time: 43 ms (7.12%), Space: 20.7 MB (8.29%) - LeetHub

--- LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,17 +1,40 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        start, end = 0, len(numbers)-1
+        # """
+        # 투포인터
+        # time: O(nlogn)
+        # space: O(1)
+        # """
+        # start, end = 0, len(numbers)-1
 
-        while start < end:
-            merge = numbers[start] + numbers[end]
+        # while start < end:
+        #     merge = numbers[start] + numbers[end]
 
-            if merge == target:
-                return [start+1, end+1]
-            elif merge < target:
-                start+=1
-            elif merge > target:
-                end-=1
+        #     if merge == target:
+        #         return [start+1, end+1]
+        #     elif merge < target:
+        #         start+=1
+        #     elif merge > target:
+        #         end-=1
         
+        """
+        이진 탐색
+        time: O(n)
+        space: O(1)
+        """
+        for i in range(len(numbers)-1):
+            complement = target-numbers[i]
+            low, high = i+1, len(numbers)-1
+
+            while low <= high:
+                mid = (low+high)//2
+
+                if numbers[mid] < complement:
+                    low=mid+1
+                elif numbers[mid] > complement:
+                    high=mid-1
+                else:
+                    return [i+1, mid+1]
 
         
