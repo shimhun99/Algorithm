ab72663a508deb69bee96a656ef8ebd6758889dd
Shim Hun <90015362+shimhun99@users.noreply.github.com>
2026-09-05 01:41:11 +0900

    Time: 0 ms (100%), Space: 19.4 MB (67.48%) - LeetHub

--- LeetCode/Medium/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ LeetCode/Medium/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -13,17 +13,21 @@ class Solution:
         return nums[0]
 
         """
-        time: O
-        space: 
+        이진 탐색
+        time: O(logn)
+        space: O(1)
         """
         low, high = 1, len(nums)-1
 
         while low <= high:
             mid = (low+high)//2
+            # 꺾인 지점 발견
             if nums[mid-1] > nums[mid]:
                 return nums[mid]
+            # nums[0]과 mid 사이의 값들은 전부 오름차순 정렬되어있음 -> 오른쪽 범위에서 꺾인 구간 찾기
             if nums[0] < nums[mid]:
                 low=mid+1
+            # nums[0]과 mid 사이에서 한번 꺾인 구간이 있음 -> 왼쪽 구간에서 nums[0]보다 큰 값을 찾기
             else:
                 high=mid-1
         return nums[0]
