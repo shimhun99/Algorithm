341294469a6cdc6467008e30dbc90825fad770c4
Shim Hun <90015362+shimhun99@users.noreply.github.com>
2026-09-22 01:26:48 +0900

    Time: 28 ms (35.58%), Space: 27.5 MB (25.75%) - LeetHub

---
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,20 @@
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        """
+        after, before에는 누적곱을 각각 저장
+
+        """
+        before = [1] * len(nums)
+        for i in range(len(nums)-1):
+            before[i+1] = before[i] * nums[i]
+        
+        after=[1]*len(nums)
+        for i in range(len(nums)-1, 0, -1):
+            after[i-1] = after[i] * nums[i]
+        
+        products=[]
+        for b,a in zip(before, after):
+            products.append(b*a)
+        
+        return products
+        
