852e63235bbfccff6222ece26314705229f772b6
Shim Hun <90015362+shimhun99@users.noreply.github.com>
2026-09-22 01:31:05 +0900

    Time: 29 ms (34.31%), Space: 25.7 MB (43.47%) - LeetHub

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,22 +1,47 @@
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
+        # """
+        # after, before에는 누적곱을 각각 저장
+        
+        # time: O(3n)=O(n)
+        # space: O(2n)=O(n)
+        # """
+        # before = [1] * len(nums)
+        # for i in range(len(nums)-1):
+        #     before[i+1] = before[i] * nums[i]
+        
+        # after=[1]*len(nums)
+        # for i in range(len(nums)-1, 0, -1):
+        #     after[i-1] = after[i] * nums[i]
+        
+        # products=[]
+        # for b,a in zip(before, after):
+        #     products.append(b*a)
+        
+        # return products
+
         """
         after, before에는 누적곱을 각각 저장
         
         time: O(3n)=O(n)
         space: O(2n)=O(n)
         """
-        before = [1] * len(nums)
+        products=[1] * len(nums)
+
+        before = 1
         for i in range(len(nums)-1):
-            before[i+1] = before[i] * nums[i]
+            before*=nums[i]
+            products[i+1]*=before
+            # before[i+1] = before[i] * nums[i]
         
-        after=[1]*len(nums)
+        after=1
         for i in range(len(nums)-1, 0, -1):
-            after[i-1] = after[i] * nums[i]
+            after*=nums[i]
+            products[i-1]*=after
+            # after[i-1] = after[i] * nums[i]
         
-        products=[]
-        for b,a in zip(before, after):
-            products.append(b*a)
+        # for b,a in zip(before, after):
+        #     products.append(b*a)
         
         return products
         
