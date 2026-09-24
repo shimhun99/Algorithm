3547d0352fb658cc4263dd5cbd705791a8ea13ea
Shim Hun <90015362+shimhun99@users.noreply.github.com>
2026-09-24 21:10:10 +0900

    Time: 52 ms (46.05%), Space: 30.8 MB (61.28%) - LeetHub

---
+++ LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive.py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstMissingPositive(self, nums: list[int]) -> int:
+        n = len(nums)
+
+        for i in range(n):
+            # nums[i]를 자기 자리로 보냄. 받아올 값도 자리가 있으면 계속 보냄
+            while 1<= nums[i] <= n and nums[nums[i]-1] != nums[i]:
+                j=nums[i]-1
+                nums[i], nums[j] = nums[j],nums[i]
+
+        for i in range(n):
+            if nums[i] != i+1:
+                return i+1
+        return n+1
+        
